e2adb468ba4f1791cc911d321a54aedf75b745a8
kt5007 <kenjin.0546@gmail.com>
2021-11-02 20:42:04 +0900

    created test data for chapter1

---
+++ tatsujin_sql/chapter1.sql
@@ -0,0 +1,28 @@
+/* テーブルの作成 */
+CREATE TABLE PopTbl
+(pref_name VARCHAR(32) PRIMARY KEY,
+    population INTEGER NOT NULL);
+
+INSERT INTO PopTbl VALUES('徳島', 100);
+INSERT INTO PopTbl VALUES('香川', 200);
+INSERT INTO PopTbl VALUES('愛媛', 150);
+INSERT INTO PopTbl VALUES('高知', 200);
+INSERT INTO PopTbl VALUES('福岡', 300);
+INSERT INTO PopTbl VALUES('佐賀', 100);
+INSERT INTO PopTbl VALUES('長崎', 200);
+INSERT INTO PopTbl VALUES('東京', 400);
+INSERT INTO PopTbl VALUES('群馬', 50);
+
+-- mysql,pstgresでのみ有効
+SELECT CASE pref_name
+    WHEN '徳島' THEN '四国'
+    WHEN '香川' THEN '四国'
+    WHEN '愛媛' THEN '四国'
+    WHEN '高知' THEN '四国'
+    WHEN '福岡' THEN '九州'
+    WHEN '佐賀' THEN '九州'
+    WHEN '長崎' THEN '九州'
+    ELSE 'その他' END AS prefectures,
+    SUM(population)
+    FROM PopTbl
+    GROUP BY prefectures;
